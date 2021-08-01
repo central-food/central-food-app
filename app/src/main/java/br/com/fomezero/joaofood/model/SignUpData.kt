6dfbab750dd8c1c7f6203e275c4f875f8823eebb
lucaomartins <lucasmariano.martins@outlook.com>
2021-08-01 20:22:35 -0300

    🔥 Remove unused data classes

--- app/src/main/java/br/com/fomezero/joaofood/model/SignUpData.kt
+++
@@ -1,9 +0,0 @@
-package br.com.fomezero.joaofood.model
-
-data class SignUpData(
-    val completeName: String,
-    val email: String,
-    val password: String,
-    val confirmPassword: String,
-    val phoneNumber: String
-)
