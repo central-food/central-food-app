2acc13c4a96c8f98d7cf4ffbd3d9f8eac318e825
lucaomartins <lucasmariano.martins@outlook.com>
2021-08-03 21:30:03 -0300

    :construction: Add basic fragments

---
+++ application/src/main/java/br/com/fomezero/centralfood/presentation/main/MainFragment.kt
@@ -0,0 +1,31 @@
+package br.com.fomezero.centralfood.presentation.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.fomezero.centralfood.R
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel by viewModel<MainViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel.initialAuthentication(requireView())
+    }
+
+}
